46fce67f8806d21be08551dab74a12f022e17f32
ryo <godbuster.of.disaster@gmail.com>
2015-03-26 16:49:19 +0100

    double switched to float

--- Gate.py
+++ Gate.py
@@ -10,18 +10,18 @@ class Gate(object):
 
         def put_coin(self):
 		coin = input("--> ")
-		self.gate_gets_paid(self, coin)
+		self.gate_gets_paid(coin)
 
 	def gate_informs(self):
 		print "Insert " + str(self.paid)
 		self.put_coin()
 	
         def gate_returns_rest(self,coin):
-                if isinstance(coin, double):
+                if isinstance(coin, float):
                         return coin-self.paid
 
 	def gate_gets_paid(self,coin):
-                if isinstance(coin, double):
+                if isinstance(coin, float):
                         if (coin == self.paid) or (coin > self.paid):
                             print "Enter. "
                             self.gate_returns_rest(self,coin)
