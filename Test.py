f183bf288020acb879162845fc11f3ab13e897e4
ryo <godbuster.of.disaster@gmail.com>
2015-03-28 21:24:23 +0100

    Test.py: instance of class Gate() added

--- Test.py
+++ Test.py
@@ -1,8 +1,10 @@
 import unittest
 import Gate
 
+gate = Gate(2.0)
+
 class GateTests(unittest.TestCase):
 	def testGate(self):
-		self.assertTrue(Gate(2.0).gate_welcomes())
+		self.assertTrue(gate.gate_welcomes())
 
 	def assertPaid(self):
